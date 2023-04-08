71843cf0698536ce86aa70f2841ad79b3845d3e1
klei30 <121291603+klei30@users.noreply.github.com>
2023-04-08 16:23:47 +0200

    Update useGenerateResult.ts

--- src/hooks/useGenerateResult.ts
+++ src/hooks/useGenerateResult.ts
@@ -1,10 +1,3 @@
-import { GenerateApiInput } from '@/utils/types'
-import { useTranslation } from 'next-i18next'
-import { useRouter } from 'next/router'
-import { useState } from 'react'
-import { toast } from 'react-hot-toast'
-import { loadLicenseKey } from './../utils/localData'
-
 export const useGenerateResult = () => {
   const router = useRouter()
   const [generatedResults, setGeneratedResults] = useState<string>('')
@@ -24,16 +17,17 @@ export const useGenerateResult = () => {
     })
 
     if (!response.ok) {
-      if (response.status === 429) {
-        toast(t('runout_today'), { icon: '🔴' })
-        router.push('/usage')
-        return
-      } else if (response.status === 439) {
-        toast(t('license_wrong'), { icon: '🔴' })
-        router.push('/usage')
-      } else {
+      // Commented out the status 429 and 439 checks
+      // if (response.status === 429) {
+      //   toast(t('runout_today'), { icon: '🔴' })
+      //   router.push('/usage')
+      //   return
+      // } else if (response.status === 439) {
+      //   toast(t('license_wrong'), { icon: '🔴' })
+      //   router.push('/usage')
+      // } else {
         throw new Error(response.statusText)
-      }
+      // }
     }
 
     // This data is a ReadableStream
