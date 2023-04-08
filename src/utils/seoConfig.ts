f7fd9baca18f0ab7468dcfb5b953483ac617df07
klei30 <121291603+klei30@users.noreply.github.com>
2023-04-08 19:10:12 +0200

    Update seoConfig.ts

--- src/utils/seoConfig.ts
+++ src/utils/seoConfig.ts
@@ -3,7 +3,7 @@ import { DefaultSeoProps } from 'next-seo'
 export const SITE_DESC =
   '立即使用海量的 ChatGPT 应用，或在几秒钟内创建属于自己的应用。'
 export const DEFAULT_SEO_CONFIG: DefaultSeoProps = {
-  title: 'OpenGPT - Create ChatGpt Application in seconds',
+  title: 'Botcraft Studio - Create ChatGpt Application in seconds',
   titleTemplate: '%s | OpenGPT',
   defaultTitle: 'OpenGPT - Create ChatGpt Application in seconds',
   description: SITE_DESC,
